73f830a10a4cacf954848edbd588802e680b0fdb
riordache <riordache93@hotmail.com>
2016-09-05 13:07:28 -0700

    Standing still.

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,8 @@ str="Hello M4650,"
 # In python you can concatentate strings using the addition(+) operator
 print(str + " my name is Varis Carey, and I'm the repo owner")
 # Add your hello statment below this line of code
+print(str + " my name is Robert Iordache and I'm a pusher, and I'm standing still.")
 print(str + " my name is Cody Smith and I'm a pusher")
-print(str + " my name is Robert Iordache and I'm a pusher")
 print(str + " my name is Andrew Bentley")
 print(str + " my name is Yunzhou Li")
 print(str + " my name is Liam Flannagan")
